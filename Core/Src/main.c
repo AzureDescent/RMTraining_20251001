c193530817dabccc7b48917dc1c5e17b2a708212
AzureDescent <drownfish723@sjtu.edu.cn>
2025-10-02 15:22:17 +0800

    fix(timer):改变了HAL_TIM_PeriodElapsedCallback到正确的位置

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -62,7 +62,14 @@ void SystemClock_Config(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-
+uint32_t count = 0;
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  if (htim == &htim1)
+  {
+    count++;
+  }
+}
 /* USER CODE END 0 */
 
 /**
