1935e48101fad8b7c3358ccfb0aed20624d79696
AzureDescent <drownfish723@sjtu.edu.cn>
2025-10-02 11:37:30 +0800

    实现timer1计数器溢出中断，在中断中改变compare值实现呼吸灯

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -59,6 +59,7 @@ void Error_Handler(void);
 /* Private defines -----------------------------------------------------------*/
 #define BUTTON_Pin GPIO_PIN_2
 #define BUTTON_GPIO_Port GPIOB
+#define BUTTON_EXTI_IRQn EXTI2_IRQn
 #define LED_G_Pin GPIO_PIN_14
 #define LED_G_GPIO_Port GPIOF
 
